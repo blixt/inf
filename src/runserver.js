914eca8167676f65bb076e7575736ec8cdd673fe
Andreas Blixt <andreas@blixt.org>
2011-05-19 23:54:40 +0200

    Make the runserver script executable.

--- src/runserver.js
+++ src/runserver.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var fs = require('fs'),
     ws = require('./vendor/websocket-server');
 
@@ -6,10 +8,12 @@ eval(fs.readFileSync('deps.js', 'utf-8'));
 
 var server = ws.createServer();
 server.addListener('connection', function (conn) {
+    console.log('New connection!');
     conn.addListener('message', function (msg) {
-        console.log(msg);
+        console.log('< ' + msg);
         server.send(msg);
     });
 });
 
+console.log('Listening on *:8080...')
 server.listen(8080);
